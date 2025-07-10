94d0414fef86dde47423ad90675a445faf42338d
Mounika <dasari.mounika@tcs.com>
2025-07-11 00:04:57 +0530

    onm

--- template.py
+++ template.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
 list_of_files = [
     "src/__init__.py",
     "src/helper.py",
+    "src/prompt.py"
     ".env",
     "requirements.txt",
     "setup.py",
