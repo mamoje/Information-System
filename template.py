82a54982681d18c668ab98d034955c4c8dfc528d
Mounika <dasari.mounika@tcs.com>
2025-07-10 23:37:04 +0530

    konb

--- template.py
+++ template.py
@@ -24,15 +24,15 @@ for filepath in list_of_files:
         filedir,filename = os.path.split(filepath)
 
 
-    if filedir !="":
+        if filedir !="":
             os.makedirs(filedir,exist_ok=True)
-            logging.info(f"Creating directory; {filedair} for the file: {filename}")
+            logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
+        if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
             with open(filepath, "w") as f:
                 pass
                 logging.info(f"Creating empty file: {filepath}")
 
 
-    else:
+        else:
             logging.info(f"{filename} is already exists")
